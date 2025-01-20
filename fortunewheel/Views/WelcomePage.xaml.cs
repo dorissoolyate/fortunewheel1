c68a7f1e1c2914da98aec1307cafeb8c241fe278
KarinaEEE <154447323+KarinaEEE@users.noreply.github.com>
2025-01-20 19:44:54 +0200

    Update WelcomePage.xaml.cs
    new

--- fortunewheel/Views/WelcomePage.xaml.cs
+++ fortunewheel/Views/WelcomePage.xaml.cs
@@ -1,9 +1,32 @@
-namespace fortunewheel.Views;
+namespace Views;
+
+
 
 public partial class WelcomePage : ContentPage
 {
-	public WelcomePage()
-	{
-		InitializeComponent();
-	}
+    public WelcomePage()
+
+    {
+        InitializeComponent();
+        StartImageRotation();
+    }
+
+    private async void OnStartClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new WheelPage());
+    }
+
+
+    private async void StartImageRotation()
+    {
+        while (true) 
+        {
+            
+            for (int i = 0; i < 360; i++)
+            {
+                RotatingImage.Rotation = i; 
+                await Task.Delay(10);
+            }
+        }
+    }
 }
