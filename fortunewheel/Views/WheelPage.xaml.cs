6aa60a3037b52931277954e9f6ec563057b155b5
Vadim Maasikmets <156168483+VadimMaasikmets@users.noreply.github.com>
2025-01-20 19:47:44 +0200

    Update WheelPage.xaml.cs
    new wheelpage.xaml.cs by vadim :)

--- fortunewheel/Views/WheelPage.xaml.cs
+++ fortunewheel/Views/WheelPage.xaml.cs
@@ -1,9 +1,38 @@
-namespace fortunewheel.Views;
+namespace Views;
+
+using System;
+using Microsoft.Maui.Controls;
+
 
 public partial class WheelPage : ContentPage
 {
-	public WheelPage()
-	{
-		InitializeComponent();
-	}
+    private Random _random;
+    private string[] _descriptions = {
+        "A new adventure awaits!", "Good luck is on your side!", "The future is bright!", "Today is your lucky day!", "You are on the right path!", "Good luck is on your side!"
+    };
+
+    public WheelPage()
+    {
+        InitializeComponent();
+        _random = new Random();
+    }
+
+    private async void OnSpinClicked(object sender, EventArgs e)
+    {
+        // Ñïèí êîëåñî (ïîâîðà÷èâàåì èçîáðàæåíèå)
+        var spinAnimation = new Animation(v => WheelImage.Rotation = v, 0, 360);
+        spinAnimation.Commit(this, "SpinAnimation", 16, 1000, Easing.Linear);
+
+        // ×åðåç 1 ñåêóíäó ïîêàçûâàåì îïèñàíèå
+        await Task.Delay(1000);
+        int randomIndex = _random.Next(_descriptions.Length);
+        await Navigation.PushAsync(new DescriptionPage(_descriptions[randomIndex]));
+    }
+
+    // Îáðàáîò÷èê äëÿ êíîïêè "Îïèñàíèå"
+    private void OnDescriptionClicked(object sender, EventArgs e)
+    {
+        // Íàïðèìåð, ïåðåõîä íà äðóãóþ ñòðàíèöó ñ îïèñàíèåì
+        DisplayAlert("Hint", "Snip the wheel :)", "OK");
+    }
 }
